b3ce02eff2269db4627eae021de56c7add8cd4ee
AplicacionLogin <garzon_luisa2004@hotmail.com>
2025-04-07 23:28:55 -0500

    Pantalla bienvenida y strings de registro

---
+++ app/src/main/java/com/example/aplicacionlogin/bienvenida.kt
@@ -0,0 +1,30 @@
+package com.example.aplicacionlogin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class bienvenida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bienvenida)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Aquí conectamos el botón INICIAR SESIÓN con el login (MainActivity)
+        val btnIniciarSesion = findViewById<Button>(R.id.button4)
+        btnIniciarSesion.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
