79659f499db328023f06360b19ef1ade06f9bc5a
luisabgm12 <Garzon_luisa2004@hotmail.com>
2025-04-10 19:07:50 -0500

    Arreglo button de registro

--- app/src/main/java/com/example/aplicacionlogin/ActivityRegistro.kt
+++ app/src/main/java/com/example/aplicacionlogin/ActivityRegistro.kt
@@ -1,11 +1,20 @@
 package com.example.aplicacionlogin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ActivityRegistro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        // Configuración del botón de redirección
+        val btnListo = findViewById<Button>(R.id.button3) // Asegúrate de que el ID coincida con el que tienes en tu layout
+        btnListo.setOnClickListener {
+            val intent = Intent(this, bienvenida::class.java) // Clase de la actividad de bienvenida
+            startActivity(intent) // Iniciar la actividad de bienvenida
+        }
     }
 }
